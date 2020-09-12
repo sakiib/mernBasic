1196336fffb1b21d1c0e907b53a56fb1f57543bc
sakibalamin <sakibalamin162@gmail.com>
2020-09-12 19:41:44 +0600

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('connected to the mongoDB!');
+}).catch(error => {
+    console.log('db connection error ${error.message}');
+    process.exit(-1);
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1> Home Page </h1>');
+});
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+
+
