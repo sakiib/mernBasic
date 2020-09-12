0986dce81274e3a499009d154ad03f66ecb516a1
sakibalamin <sakibalamin162@gmail.com>
2020-09-12 21:26:26 +0600

    react init cleanup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <div className="container">
+      <Home />
+     <h3> Hello From React </h3>
+    </div>
+  );
+};
+
+export default App;
